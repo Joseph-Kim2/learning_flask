6b15f7512e0a9a8b554f52a34539f0dff895f2da
Joseph Kim <jkim7004@gmail.com>
2024-04-08 19:12:23 -0500

    changed error on app.py and added about content

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 
 @app.route("/about")
 def about():
-    return about.html
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
